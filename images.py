e16d67aeb2c8729c9e1213ce18ee5fde665274cd
kal <kapi.zaorski@gmail.com>
2023-07-25 13:04:03 +0200

    :(

--- images.py
+++ images.py
@@ -11,4 +11,5 @@ car_image_mirrored = pygame.image.load("images/car_mirrored.png").convert_alpha(
 heart_image = pygame.image.load("images/heart.png").convert_alpha()
 hunger_image = pygame.image.load("images/hunger.png").convert_alpha()
 logo_image = pygame.image.load("images/logo.png").convert()
-
+inventory_slot_image = pygame.image.load("images/inventory_slot.png")
+bread_image = pygame.image.load("images/bread.png")
